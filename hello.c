c5ac28944e89f233ce7057794ff39a38e7148b21
Shivanshu Sharma <shivanshusharma@Shivanshus-MacBook-Air.local>
2025-02-08 16:48:17 +0530

    Just made few change more at 4:47PM on 8-feb-2025.

--- hello.c
+++ hello.c
@@ -5,4 +5,4 @@
   
   return 0;
 }
-//so i have created this file just for learning purpose and just to learnthat how to generate  push pull  requeat.
+//so, I have created this file just for learning purpose and just to learnthat how to generate  push pull  requeat.
